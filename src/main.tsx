9b72166910031e3f4791bd37ae59b7184b402902
missxiaolin <462441355@qq.com>
2025-02-25 10:04:38 +0800

    feat: 打包优化

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "@ant-design/v5-patch-for-react-19"; // fix for react 19
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,68 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePluginExternals } from "vite-plugin-externals-new";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // build: {
+  //   outDir: './dist',
+  // },
+  // server: {
+  //   host: '127.0.0.1',
+  //   port: 8080,
+  //   headers: {
+  //     'Access-Control-Allow-Origin': '*',
+  //   },
+  //   proxy: {
+  //     '/api': {
+  //       // 如果本地启动后端，请替换为后端地址
+  //       target: '',
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    process.env.NODE_ENV === "production" &&
+      VitePluginExternals({
+        // react: {
+        //   src: '/static/lib/',
+        //   varName: 'React',
+        // },
+        // 'react-dom': {
+        //   src: '/static/lib/',
+        //   varName: 'ReactDOM',
+        // },
+        dayjs: {
+          src: "/static/lib/dayjs.min.js",
+          varName: "dayjs",
+        },
+        antd: {
+          src: "/static/lib/antd.min.js",
+          varName: "antd",
+        },
+        ahooks: {
+          src: "/static/lib/ahooks.min.js",
+          varName: "ahooks",
+        },
+        axios: {
+          src: "/static/lib/axios.min.js",
+          varName: "axios",
+        },
+        "@ant-design/icons": {
+          src: "/static/lib/ant-design-icons.min.js",
+          varName: "icons",
+        },
+        "@ant-design/plots": {
+          src: "/static/lib/plots.min.js",
+          varName: "Plots",
+        },
+      }),
+  ],
 });
